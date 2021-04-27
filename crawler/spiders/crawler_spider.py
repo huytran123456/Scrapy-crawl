5635e35f355ba5c1beeab40911cc8faf3880b92f
LAPTOP-NP1GDI8O\hi <trandinhhuy1996tv@gmail.com>
2021-04-27 19:49:51 +0700

    first

--- crawler/spiders/crawler_spider.py
+++ crawler/spiders/crawler_spider.py
@@ -4,26 +4,40 @@ from scrapy import Spider
 from scrapy.selector import Selector
 from crawler.items import CrawlerItem
 import json
-
-
+from scrapy.http import Request
+import scrapy
 class CrawlerSpider(Spider):
     name = "test"
     # List your urls to crawl here!!!
+    base_Url = "https://vneconomy.vn"
     start_urls = [
         "https://vneconomy.vn/ong-vu-tien-loc-lan-song-fdi-the-he-moi-khong-chay-theo-so-luong-quy-mo-646514.htm",
-        "https://vneconomy.vn/nguy-co-bi-kiem-soat-dinh-gia-cua-ant-group-co-the-chi-dang-29-ty-usd-thay-vi-320-ty-646521.htm",
-        "https://vneconomy.vn/giai-ma-nguon-con-cat-lo-chung-cu-cao-cap-ha-noi-ca-ty-dong-moi-can-646061.htm"
-    ]
 
+    ]
+    allowed_domains = ["vneconomy.vn"]
     def parse(self, response):
         # yield {"title":response.css('div.detail__author::text')[0].get()}
         for t in response.css('header.detail__header'):
+            # self.content(t)
             yield {"URL": response.request.url,
                    "Title": t.css('h1.detail__title::text').get(),
                    "Author": t.xpath('//div[@class="detail__author"]/strong/text()').get(),
                    "Date": t.css('div.detail__meta::text').get()}
-          # print(title.css('::text').get())
+        for href in response.xpath('//article[@class="story story--featured"]/figure[@class="story__thumb"]/a/@href').extract():
+            #print(self.base_Url+href)
+            yield scrapy.Request(self.base_Url+href,callback=self.content)
+            # yield {
+            #     "URL": self.base_Url + href
+            # }
+           # print(title.css('::text').get())
         # item['Author'] = Selector(response).xpath(
         #     'span[@class="ReferenceSourceTG"]/text()').extract_first()
 
         # yield item
+    def content(self, response):
+        for t in response.css('header.detail__header'):
+            # self.content(t)
+            yield {"URL": response.request.url,
+                   "Title": t.css('h1.detail__title::text').get(),
+                   "Author": t.xpath('//div[@class="detail__author"]/strong/text()').get(),
+                   "Date": t.css('div.detail__meta::text').get()}
